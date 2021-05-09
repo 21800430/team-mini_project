a995db382715c51fef8de4b1417a3e998e113d07
21800430 <82192965+21800430@users.noreply.github.com>
2021-05-09 14:20:50 +0900

    update delete

--- project/product.c
+++ project/product.c
@@ -104,7 +104,7 @@ int selectBookNum(Book *b[], int index) {
     booklist(b, index, -1);
     int num;
     
-    printf("\n원하는 책의 번호를 입력해주세요. (취소 : 0)");
+    printf("\n원하는 책의 번호를 입력해주세요.");
     scanf("%d", &num);
     
     if(b[num] == NULL) {
@@ -115,15 +115,20 @@ int selectBookNum(Book *b[], int index) {
 }
 
 // 원하는 책의 정보를 삭제하는 함수
-int deleteBook(Book *b){
+int deleteBook(Book *b, int index){
+    int no = selectBookNum(b, index);
     int delOk;
-    printf("삭제하시려면 1번을 눌러주세요. ");
-    scanf("%d", &delOk);
-    if(delOk == 1) {
-        free(b);
-        b = NULL;
-        count--;
-        printf("=> 삭제되었습니다.\n");
+    if(b[no] != NULL) {
+        printf("삭제하시려면 1번을 눌러주세요. ");
+        scanf("%d", &delOk);
+        if(delOk == 1) {
+            free(b[no]);
+            b[no] = NULL;
+            count--;
+            printf("=> 삭제되었습니다.\n");
+        } else {
+            printf("=> 취소되었습니다.\n");
+        }
     } else {
         printf("=> 취소되었습니다.\n");
     }
